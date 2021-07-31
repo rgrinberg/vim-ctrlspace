5b7dd68b4279a475f2bff034cc766ae9a19e076d
Rudi Grinberg <me@rgrinberg.com>
2021-07-31 00:31:36 -0700

    rewrite engine in lua

--- lua/ctrlspace.lua
+++ lua/ctrlspace.lua
@@ -134,6 +134,14 @@ local function raw_buffers_in_tab(tabnr)
   return vim.fn.gettabvar(tabnr, "CtrlSpaceList", {})
 end
 
+local function buffers_in_tab(tabnr)
+  local res = {}
+  for key, _ in pairs(raw_buffers_in_tab(tabnr)) do
+    res[tonumber(key)] = true
+  end
+  return res
+end
+
 -- TODO this should exist in the tabs module
 -- TODO sort
 buffers.in_tab = function (tabnr)
@@ -292,6 +300,202 @@ local function help_filler()
   return fill
 end
 
+local item = {}
+
+item.create = function(index, text, indicators)
+  return {
+    index = index,
+    text = text,
+    indicators = indicators,
+  }
+end
+
+local function bookmark_items()
+  local config = vim.fn["ctrlspace#context#Configuration"]()
+
+  local bookmarks = vim.fn['ctrlspace#bookmarks#Bookmarks']()
+  local active = vim.fn['ctrlspace#bookmarks#FindActiveBookmark']()
+
+  local res = {}
+  for i, bm in ipairs(bookmarks) do
+    local indicators = ""
+    if active and bm.Directory == active.Directory then
+      indicators = config.Symbols.IA
+    end
+    table.insert(res, item.create(i, bm.Name, indicators))
+  end
+  return res
+end
+
+local function workspace_items (clv)
+  local workspaces = vim.fn["ctrlspace#workspaces#Workspaces"]()
+  local active = vim.fn["ctrlspace#workspaces#ActiveWorkspace"]()
+  local config = vim.fn["ctrlspace#context#Configuration"]()
+
+  local res = {}
+  for i, ws in ipairs(workspaces) do
+    local indicators = ""
+    if active and ws == active.Name and active.Status ~= 0 then
+      if active.Status == 2 then
+        indicators = indicators .. config.Symbols.IM
+      end
+    elseif ws == clv.Data.LastActive then
+      indicators = indicators .. config.Symbols.IV
+    end
+    table.insert(item.create(i, ws, indicators))
+  end
+  return res
+end
+
+local function tab_items()
+  local config = vim.fn["ctrlspace#context#Configuration"]()
+  local current_tab = vim.fn.tabpagenr()
+
+  local res = {}
+  for tabnr=1,vim.fn.tabpagenr("$") do
+    local indicators = ""
+
+    local tab_buffer_number = vim.fn["ctrlspace#api#TabBuffersNumber"](tabnr)
+    local title = vim.fn["ctrlspace#api#TabTitle"](tabnr)
+
+    if vim.fn["ctrlspace#api#TabModified"](tabnr) ~= 1 then
+      indicators = indicators .. config.Symbols.IM
+    end
+
+    if tabnr == current_tab then
+      indicators = indicators .. config.Symbols.IA
+    end
+
+    local name = tabnr .. " " .. tab_buffer_number .. " " .. title
+    table.insert(res, item.create(tabnr, name, indicators))
+  end
+  return res
+end
+
+local function buffer_items(clv)
+  local res = {}
+  local bufs
+  local submode = clv.Data.SubMode
+  local config = vim.fn["ctrlspace#context#Configuration"]()
+
+  if submode == "single" then
+    bufs = buffers_in_tab(vim.fn.tabpagenr())
+  elseif submode == "all" then
+    bufs = all_buffers()
+  elseif submode == "visible" then
+    bufs = {}
+    for buf, _ in raw_buffers_in_tab(vim.fn.tabeagenr()) do
+      if vim.fn.bufwinnr(buf) ~= -1 then
+        bufs[buf] = true
+      end
+    end
+  else
+    error("invalid mode " .. submode)
+  end
+
+  for bufnr, _ in pairs(bufs) do
+    local name = vim.fn.fnamemodify(vim.fn.bufname(bufnr), ":.")
+    local modified = getbufvar(bufnr, "&modified") == 1
+    local winnr = vim.fn.bufwinnr(bufnr)
+
+    if name == "" and (modified or winnr ~= -1) then
+      name = "[" .. bufnr .. "*No Name]"
+    end
+
+    local indicators = ""
+    if modified then
+      indicators = indicators .. config.Symbols.IM
+    end
+
+    if winnr == vim.t.CtrlSpaceStartWindow then
+      indicators = indicators .. config.Symbols.IA
+    elseif winnr ~= -1 then
+      indicators = indicators .. config.Symbols.IV
+    end
+
+    table.insert(res, item.create(bufnr, name, indicators))
+  end
+
+  return res
+end
+
+local function content_source()
+  local clv = vim.fn["ctrlspace#modes#CurrentListView"]()
+
+  if clv.Name == "Buffer" then
+    return buffer_items(clv)
+  elseif clv.Name == "File" then
+    return vim.fn["ctrlspace#files#CollectFiles"]()
+  elseif clv.Name == "Tab" then
+    return tab_items()
+  elseif clv.Name == "Workspace" then
+    return workspace_items(clv)
+  elseif clv.Name == "Bookmark" then
+    return bookmark_items()
+  else
+    error("unknown list view: " .. clv.Name)
+  end
+end
+
+local function render_candidates(items)
+  local config = vim.fn["ctrlspace#context#Configuration"]()
+  local modes = vim.fn["ctrlspace#modes#Modes"]()
+  local sizes = vim.fn["ctrlspace#context#SymbolSizes"]()
+
+  local item_space
+  if modes.File.Enabled == 1 then
+    item_space = 5
+  elseif modes.Bookmark.Enabled == 1 then
+    item_space = 5 + sizes.IAV
+  else
+    item_space = 5 + sizes.IAV + sizes.IM
+  end
+
+  local res = {}
+  local columns = vim.o.columns
+
+  for _, i in ipairs(items) do
+    local line = i.text
+    local len = string.len(line)
+
+    if len + item_space > columns then
+      line = config.Symbols.Dots .. string.sub(line, line - columns + item_space + sizes.Dots)
+    end
+
+    if i.indicators ~= "" then
+      line = line .. " " .. i.indicators
+    end
+    line = "  " .. line .. "\n"
+
+    table.insert(res, line)
+  end
+
+  return table.concat(res, "")
+end
+
+drawer.content = function ()
+  local absolute_max = 500
+  local candidates = content_source()
+  local modes = vim.fn["ctrlspace#modes#Modes"]()
+  local query = table.concat(modes.Search.Data.Letters, "")
+  if query == "" then
+    if #candidates > absolute_max then
+      candidates = { unpack(candidates, 1, absolute_max) }
+    end
+  else
+    local max
+    if modes.Search.Enabled == 1 then
+      max = vim.fn["ctrlspace#window#MaxHeight"]()
+    else
+      max = absolute_max
+    end
+    candidates = ctrlspace_filter(candidates, query, max)
+  end
+
+  local content = render_candidates(candidates)
+  return {candidates, content}
+end
+
 drawer.insert_content = function ()
   local config = vim.fn["ctrlspace#context#Configuration"]()
   local modes = vim.fn["ctrlspace#modes#Modes"]()
@@ -302,7 +506,7 @@ drawer.insert_content = function ()
     return
   end
 
-  local content = vim.fn["ctrlspace#engine#Content"]()
+  local content = drawer.content()
   local items = content[1]
   local text = content[2]
 
@@ -321,7 +525,7 @@ drawer.insert_content = function ()
     vim.cmd('silent! exe "resize "' .. size)
   end
 
-  vim.cmd('silent! exe "set updatetime="' .. config.SearchTiming)
+  vim.o.updatetime = config.SearchTiming
 
   vim.fn["ctrlspace#window#displayContent"](items, text)
   vim.fn["ctrlspace#util#SetStatusline"]()
